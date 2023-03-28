71349f864fbefbe7f326d7f86d5f9951d333d4ba
Mohamed Ebeid <mohmedalhlim@gmail.com>
2023-03-28 16:32:39 +0300

    Viewing courses is now does not require being an admin

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -4,10 +4,7 @@ import Course from "../Models/courseModel.js";
 export const courses = async (req, res) => {
 	try {
 		const courses = await Course.find();
-		//authorization, we could have used it as middleware but since it's a small backend I didn't see the need to
-		if (req.headers && req.headers.auth !== "admin") {
-			return res.status(401).send("Unauthorized access!");
-		}
+
 		if (courses.length === 0) {
 			return res.status(404).send("No courses were found!");
 		} else {
@@ -20,6 +17,7 @@ export const courses = async (req, res) => {
 
 //Adding a new curse
 export const postCourse = async (req, res) => {
+	//authorization, we could have used it as middleware but since it's a small backend I didn't see the need to
 	if (req.headers && req.headers.auth !== "admin") {
 		return res.status(401).send("Unauthorized access!");
 	}
