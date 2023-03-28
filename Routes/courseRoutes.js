1e7dba1dfdbcfffa1da3bf212c288c3cc16b8657
Mohamed Ebeid <mohmedalhlim@gmail.com>
2023-03-28 14:15:53 +0300

    added deleting and some comments

--- Routes/courseRoutes.js
+++ Routes/courseRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { courses, postCourse } from "../controllers/courseController.js";
+import {
+  courses,
+  postCourse,
+  deleteCourse,
+} from "../controllers/courseController.js";
 
 const courseRouter = express.Router();
 
 //api for getting all courses
 courseRouter.get("/", courses);
 courseRouter.post("/", postCourse);
+courseRouter.delete("/:courseId", deleteCourse);
 
 export default courseRouter;
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,10 @@
 import Course from "../Models/courseModel.js";
 
+//Getting all the courses
 export const courses = async (req, res) => {
 	try {
 		const courses = await Course.find();
+		//authorization, we could have used it as middleware but since it's a small backend I didn't see the need to
 		if (req.headers && req.headers.auth !== "admin") {
 			return res.status(401).send("Unauthorized access!");
 		}
@@ -12,10 +14,11 @@ export const courses = async (req, res) => {
 			return res.send(courses);
 		}
 	} catch (e) {
-		return res.status(400).send("Error. Try again.");
+		return res.status(400).send(e.message);
 	}
 };
 
+//Adding a new curse
 export const postCourse = async (req, res) => {
 	if (req.headers && req.headers.auth !== "admin") {
 		return res.status(401).send("Unauthorized access!");
@@ -26,6 +29,19 @@ export const postCourse = async (req, res) => {
 		}).save();
 		res.json(course);
 	} catch (e) {
-		return res.status(400).send("Error. Try again.");
+		return res.status(400).send(e.message);
+	}
+};
+
+//Deleting a course
+export const deleteCourse = async (req, res) => {
+	if (req.headers && req.headers.auth !== "admin") {
+		return res.status(401).send("Unauthorized access!");
+	}
+	try {
+		const deleted = await Course.findByIdAndRemove(req.params.courseId);
+		res.json("Deleted successfully! ");
+	} catch (e) {
+		return res.status(400).send(e.message);
 	}
 };
