5a694d74e6cdaf5de355f47c4ecb8b5d1010071d
Mohamed Ebeid <mohmedalhlim@gmail.com>
2023-03-29 14:02:12 +0300

    Getting a single course is added 02

--- Routes/courseRoutes.js
+++ Routes/courseRoutes.js
@@ -9,11 +9,11 @@ import {
 
 const courseRouter = express.Router();
 
-//api for getting all courses
-courseRouter.get("/", courses);
-courseRouter.get("/:courseId", course);
-courseRouter.post("/", postCourse);
-courseRouter.delete("/:courseId", deleteCourse);
-courseRouter.put("/:courseId", updateCourse);
+//api courses
+courseRouter.get("/", courses); //all
+courseRouter.get("/:courseId", course); //single
+courseRouter.post("/", postCourse); //add
+courseRouter.delete("/delete/:courseId", deleteCourse); //delete
+courseRouter.put("/add/:courseId", updateCourse); //update
 
 export default courseRouter;
